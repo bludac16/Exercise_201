cb36e2819f481be761b5c3d1b19dc5145330e349
wimmac <wimmac16@new.edvhtl.at>
2018-10-04 08:17:39 +0200

    implemented Model

--- Exercise_201/src/VelocityTableModel.java
+++ Exercise_201/src/VelocityTableModel.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +12,23 @@
  *
  * @author David
  */
-public class VelocityTableModel {
+public class VelocityTableModel extends AbstractListModel<Measurement>{
+    
+    private ArrayList<Measurement> measurments=new ArrayList<>();
+    
+        public void add(Measurement m) {
+        measurments.add(m);
+        fireIntervalAdded(this, measurments.size() - 1, measurments.size() - 1);
+    }
+        
+    @Override
+    public int getSize() {
+        return measurments.size();
+    }
+
+    @Override
+    public Measurement getElementAt(int index) {
+      return measurments.get(index);
+    }
     
 }
